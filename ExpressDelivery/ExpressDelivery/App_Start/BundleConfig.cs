39a4580f2ca25518275b47dcbc75664967375c23
AndreyRakhmaev <andrey.rakhmaev@gmail.com>
2017-06-01 02:00:29 +0600

    Express Delivery

--- ExpressDelivery/ExpressDelivery/App_Start/BundleConfig.cs
+++ ExpressDelivery/ExpressDelivery/App_Start/BundleConfig.cs
@@ -24,45 +24,45 @@ namespace ExpressDelivery
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/bundles/bootstrap-core-css").Include(
-                      "~/Content/css/bootstrap.css",
-                      "~/Content/css/theme.css",
-                      "~/Content/css/bootstrap-reset.css"));
+                      "~/Content/css/bootstrap*",
+                      "~/Content/css/theme*",
+                      "~/Content/css/bootstrap-reset*"));
             bundles.Add(new StyleBundle("~/bundles/external-css").Include(
-                      "~/Content/assets/font-awesome/css/font-awesome.css",
-                      "~/Content/css/flexslider.css",
-                      "~/Content/assets/bxslider/jquery.bxslider.css",
-                      "~/Content/assets/owlcarousel/owl.carousel.css",
-                      "~/Content/assets/owlcarousel/owl.theme.css",
-                      "~/Content/css/superfish.css",
+                      "~/Content/assets/font-awesome/css/font-awesome*",
+                      "~/Content/css/flexslider*",
+                      "~/Content/assets/bxslider/jquery.bxslider*",
+                      "~/Content/assets/owlcarousel/owl.carousel*",
+                      "~/Content/assets/owlcarousel/owl.theme*",
+                      "~/Content/css/superfish*",
                       "~/http://fonts.googleapis.com/css?family=Lato",
-                      "~/Content/css/parallax-slider/parallax-slider.css",
-                      "~/Content/css/animate.css"));
-            bundles.Add(new StyleBundle("~/bundles/custom-styles-css").Include(
-                      "~/Content/css/component.css",
-                      "~/Content/css/style.css",
-                      "~/Content/css/style-responsive.css",
-                      "~/Content/css/Site.css"));
+                      "~/Content/css/parallax-slider/parallax-slider*",
+                      "~/Content/css/animate*"));
+            bundles.Add(new StyleBundle("~/bundles/custom-styles*").Include(
+                      "~/Content/css/component*",
+                      "~/Content/css/style*",
+                      "~/Content/css/style-responsive*",
+                      "~/Content/css/Site*"));
 
 
 
             bundles.Add(new ScriptBundle("~/bundles/custom-js").Include(
-                        "~/Content/js/parallax-slider/modernizr.custom.28468.js",
-                        "~/Content/js/parallax-slider/modernizr.custom.28468.js"));
+                        "~/Content/js/parallax-slider/modernizr.custom.28468*",
+                        "~/Content/js/parallax-slider/modernizr.custom.28468*"));
             bundles.Add(new ScriptBundle("~/bundles/external-js").Include(
-                        "~/Content/js/hover-dropdown.js",
-                        "~/Content/assets/bxslider/jquery.bxslider.js",
-                        "~/Content/js/jquery.parallax-1.1.3.js",
-                        "~/Content/js/wow.min.js",
-                        "~/Content/assets/owlcarousel/owl.carousel.js",
-                        "~/Content/js/jquery.easing.min.js",
-                        "~/Content/js/link-hover.js",
-                        "~/Content/js/superfish.js",
-                        "~/Content/js/parallax-slider/jquery.cslider.js",
-                        "~/Content/js/jquery.flexslider.js"));
-            bundles.Add(new ScriptBundle("~/bundles/common-js").Include(
-                        "~/Content/js/common-scripts.js"));
+                        "~/Content/js/hover-dropdown*",
+                        "~/Content/assets/bxslider/jquery.bxslider*",
+                        "~/Content/js/jquery.parallax-1.1.3*",
+                        "~/Content/js/wow.min*",
+                        "~/Content/assets/owlcarousel/owl.carousel*",
+                        "~/Content/js/jquery.easing.min*",
+                        "~/Content/js/link-hover*",
+                        "~/Content/js/superfish*",
+                        "~/Content/js/parallax-slider/jquery.cslider*",
+                        "~/Content/js/jquery.flexslider*"));
+            bundles.Add(new ScriptBundle("~/bundles/common*").Include(
+                        "~/Content/js/common-scripts*"));
             bundles.Add(new ScriptBundle("~/bundles/sliding-form").Include(
-                  "~/Content/js/sliding.form.js"));
+                  "~/Content/js/sliding.form*"));
 
             BundleTable.EnableOptimizations = true;
         }
