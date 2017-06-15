3ec1794638274a6c8bdda150456c308cac4cfb25
AndreyRakhmaev <andrey.rakhmaev@gmail.com>
2017-06-15 07:50:17 +0600

    ED

--- ExpressDelivery/ExpressDelivery/Models/OrderDetails.cs
+++ ExpressDelivery/ExpressDelivery/Models/OrderDetails.cs
@@ -8,22 +8,37 @@ namespace ExpressDelivery.Models
     public class OrderDetails
     {
         public int OrderNumber { get; set; }
+        [Required(ErrorMessage = "Введите ФИО отправителя")]
         public string FioFrom { get; set; }
+        [Required(ErrorMessage = "Введите город отправления")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Введите адрес отправителя")]
         public string AdressFrom { get; set; }
         public string ZipFrom { get; set; }
+        [Required(ErrorMessage = "Введите номер телефона отправителя")]
         public string PhoneNumberFrom { get; set; }
+        [Required(ErrorMessage = "Введите адрес эл. почты отправителя")]
         public string EmailFrom { get; set; }
 
+        [Required(ErrorMessage = "Введите ФИО получателя")]
         public string FioTo { get; set; }
+        [Required(ErrorMessage = "Введите город назначения")]
+        public string CityTo { get; set; }
+        [Required(ErrorMessage = "Введите адрес получателя")]
         public string AdressTo { get; set; }
         public string ZipTo { get; set; }
+        [Required(ErrorMessage = "Введите телефонный номер получателя")]
         public string PhoneNumberTo { get; set; }
         public string EmailTo { get; set; }
 
         public string TrackingNumber { get; set; }
+        [Required(ErrorMessage = "Введите габариты груза")]
         public string DimensionsDelivery { get; set; }
+        [Required(ErrorMessage = "Введите массу груза")]
         public double WeightDelivery { get; set; }
+        [Required(ErrorMessage = "Введите тип груза")]
         public string TypeDelivery { get; set; }
+        [Required(ErrorMessage = "Введите тарифный план")]
         public string Plan { get; set; }
         public string Comment { get; set; }
         public string Services { get; set; }
