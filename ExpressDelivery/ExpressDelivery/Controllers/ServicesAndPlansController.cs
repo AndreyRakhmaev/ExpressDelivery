4ee748c76c78b09bedeaaa70a78ad03b8e62418d
AndreyRakhmaev <andrey.rakhmaev@gmail.com>
2017-06-25 18:05:00 +0600

    ED

---
+++ ExpressDelivery/ExpressDelivery/Controllers/ServicesAndPlansController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpressDelivery.Controllers
+{
+    public class ServicesAndPlansController : Controller
+    {
+        public ActionResult Plans()
+        {
+            return View();
+        }
+    }
+}
