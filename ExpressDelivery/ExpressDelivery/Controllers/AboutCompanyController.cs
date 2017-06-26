8658550cf6efb4748cf66437852a2cc2a5cf90a6
AndreyRakhmaev <andrey.rakhmaev@gmail.com>
2017-06-27 01:10:22 +0600

    ED

--- ExpressDelivery/ExpressDelivery/Controllers/AboutCompanyController.cs
+++ ExpressDelivery/ExpressDelivery/Controllers/AboutCompanyController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ExpressDelivery.Content
+namespace ExpressDelivery.Controllers
 {
     public class AboutCompanyController : Controller
     {
